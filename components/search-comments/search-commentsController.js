2aeb340f4d264db6c79e4868d29daebe71eb86c4
YimingChen-stanford <YimingChen-stanford@users.noreply.github.com>
2016-04-13 11:57:48 -0700

    Update search-commentsController.js

--- components/search-comments/search-commentsController.js
+++ components/search-comments/search-commentsController.js
@@ -3,8 +3,7 @@
 cs142App.controller('SearchCommentsController', ['$scope', '$routeParams','$rootScope', '$location', '$resource',
   function($scope, $routeParams,$rootScope, $location, $resource) {
     /*
-     * Since the route is specified as '/photos/:userId' in $routeProvider config the
-     * $routeParams  should have the userId property set with the path from the URL.
+     * This has not been implemented
      */
    
   }]);
